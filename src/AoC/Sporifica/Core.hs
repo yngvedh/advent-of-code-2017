d6ccd5706c1ced35c44a3077036691a3f9f7ef6c
Yngve Devik Hammersland <yngve@me.com>
2018-10-21 23:51:31 +0200

    Refactor: introduce name injectCoordinates ... for complicated expression which injects coordinates into the list of nodes.

--- src/AoC/Sporifica/Core.hs
+++ src/AoC/Sporifica/Core.hs
@@ -13,10 +13,11 @@ data Node = Clean | Infected
 makeGrid :: [[Node]] -> Nodes
 makeGrid cs = Nodes . S.fromList $ assocList where
   assocList :: [((Int,Int),Node)]
-  assocList = concat . zipWith f [(-h')..h'] . map (zip [(-w')..w']) $ cs
+  assocList = concat . injectCoordinates $ cs
   w = length . head $ cs
   h = length cs
   w' = (w-1) `div` 2
   h' = (h-1) `div` 2
+  injectCoordinates = zipWith f [(-h')..h'] . map (zip [(-w')..w'])
   f :: Int -> [(Int, Node)] -> [((Int,Int),Node)]
   f y = map (\(x, c) -> ((x, y), c))
